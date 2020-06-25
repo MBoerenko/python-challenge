c63cd2265200f75f58aef98be3ce298f74fd8db4
Marianne B <mboerenko@cfl.rr.com>
2020-06-24 20:02:38 -0400

    Add Final Homework

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,3 +1,95 @@
-#import the dependencies
-import os
 import csv
+import os
+import statistics
+
+
+#set variables
+row_counter = 0
+net_total = 0
+change = 0
+change_amounts = []
+dates_change = []
+average_change = 0
+max_change = 0
+min_change = 0
+amount1 = 0
+amount2 = 0
+max_date = ""
+min_date = ""
+
+
+# Path to collect data from the Resources folder
+budget_csv_path = os.path.join('Resources', 'budget_data.csv')
+
+with open(budget_csv_path, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    
+    header = next(csvreader)
+
+    #read one line at a time
+    for row in csvreader:
+
+        # The total number of months included in the dataset
+        row_counter = row_counter + 1
+
+        # The net total amount of "Profit/Losses" over the entire period
+        net_total = net_total + int(row[1])
+
+        #create a variable to hold the amounts from column 2
+        amount2 = int(row[1])
+
+        #create a variable to hold the dates from column 1
+        date = str(row[0])
+
+        #calculate the amount of change from one row to the next    
+        change = int(amount2) - int(amount1)
+
+        #Delete the value of row 1 since 0 to row 1 is not a change in value
+        if amount1 == 0:
+            change = change - int(amount2)
+
+        #add the change to the list of change amounts
+        change_amounts.append(change)
+
+        #add the date to the list of dates
+        dates_change.append(date)
+
+        #change the value of amount 1 to equal the end value from the previous change
+        amount1 = amount2
+
+# The greatest increase in profits (date and amount) over the entire period
+max_change = max(change_amounts)
+max_date = dates_change[change_amounts.index(max_change)]
+
+# The greatest decrease in losses (date and amount) over the entire period
+min_change = min(change_amounts)
+min_date = dates_change[change_amounts.index(min_change)]
+
+# The average of the changes in "Profit/Losses" over the entire period / subtract 1 from the row-counter to 
+    #exclude row 1
+average_change = sum(change_amounts) / int(row_counter-1)
+average_change = round(average_change,2) 
+
+print("")
+print("")
+print("Financial Analysis")
+print("------------------------------")
+print("Total Months:  " + str(row_counter))
+print("Total:  $" + str(net_total))
+print("Average:  $" + str(average_change))
+print("Greatest Increase in Profits:  "+ str(max_date) + " ($" + str(max_change) + ") ")
+print("Greatest Decrease in Profits:  "+ str(min_date) + " ($" + str(min_change) + ") ")
+print("")
+
+with open("Analysis/financial.txt", "w") as text_file:
+    text_file.write("")
+    text_file.write("")
+    text_file.write("Financial Analysis")
+    text_file.write("------------------------------")
+    text_file.write("Total Months:  " + str(row_counter))
+    text_file.write("Total:  $" + str(net_total))
+    text_file.write("Average:  $" + str(average_change))
+    text_file.write("Greatest Increase in Profits:  "+ str(max_date) + " ($" + str(max_change) + ") ")
+    text_file.write("Greatest Decrease in Profits:  "+ str(min_date) + " ($" + str(min_change) + ") ")
+    text_file.write("")
+    text_file.close()
---
+++ PyPoll/main.py
@@ -0,0 +1,104 @@
+import csv
+import os
+import statistics
+
+
+#set variables
+row_counter = 0
+candidate = ""
+total_votes = 0
+max_name = ""
+max_votes = 0
+vote_percentage = 0
+khan_votes = 0
+correy_votes = 0
+li_votes = 0
+otooley_votes = 0
+khan_percentage = 0
+correy_percentage = 0
+li_percentage = 0
+otooley_percentage = 0
+
+# Path to collect data from the Resources folder
+election_csv_path = os.path.join('Resources', 'election_data.csv')
+
+with open(election_csv_path, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    
+    header = next(csvreader)
+
+    #read one line at a time
+    for row in csvreader:
+
+        # The total number of votes cast
+        row_counter = row_counter + 1
+
+        #create a variable to hold the candidate names
+        candidate = str(row[2])
+        
+        #A complete list of candidates who received votes
+        if candidate =="Khan":
+            khan_votes = khan_votes + 1
+
+        if candidate == "Correy":
+            correy_votes = correy_votes + 1
+
+        if candidate == "Li": 
+            li_votes = li_votes + 1            
+
+        if candidate == "O'Tooley":
+            otooley_votes = otooley_votes + 1
+
+    if khan_votes > correy_votes and khan_votes > li_votes and khan_votes > otooley_votes:
+        max_votes = khan_votes
+        max_name = ("Khan")
+
+    if correy_votes > khan_votes and correy_votes > li_votes and correy_votes > otooley_votes:
+        max_votes = correy_votes
+        max_name = ("Correy")
+
+    if li_votes > khan_votes and li_votes > correy_votes and li_votes > otooley_votes:
+        max_votes = li_votes
+        max_name = ("Li")
+
+        
+    if otooley_votes > khan_votes and otooley_votes > correy_votes and otooley_votes > li_votes:
+        max_votes = otooley_votes
+        max_name = ("O'Tooley")
+
+total_votes = khan_votes + correy_votes + li_votes + otooley_votes      
+khan_percentage = round((khan_votes / total_votes) * 100,2)
+correy_percentage = round((correy_votes / total_votes) * 100,2)
+li_percentage = round((li_votes / total_votes) * 100,2)
+otooley_percentage = round((otooley_votes / total_votes) * 100,2) 
+  
+print("")
+print("")
+print("Election Results")
+print("-------------------------")
+print("Total Votes:  " + str(row_counter))
+print("-------------------------")
+print("Khan:  " + str(khan_percentage) + "%  (" + str(khan_votes) + ")")
+print("Correy:  " + str(correy_percentage) + "%  (" + str(correy_votes) + ")")
+print("Li:  " + str(li_percentage) + "%  (" + str(li_votes) + ")")
+print("O'Tooley:  " + str(otooley_percentage) + "%  (" + str(otooley_votes) + ")")
+print("-------------------------")
+print("Winner:  " + str(max_name))
+print("-------------------------")
+
+with open("Analysis/polls.txt", "w") as text_file:
+    text_file.write("")
+    text_file.write("")
+    text_file.write("")
+    text_file.write("Election Results")
+    text_file.write("-------------------------")
+    text_file.write("Total Votes:  " + str(row_counter))
+    text_file.write("-------------------------")
+    text_file.write("Khan:  " + str(khan_percentage) + "%  (" + str(khan_votes) + ")")
+    text_file.write("Correy:  " + str(correy_percentage) + "%  (" + str(correy_votes) + ")")
+    text_file.write("Li:  " + str(li_percentage) + "%  (" + str(li_votes) + ")")
+    text_file.write("O'Tooley:  " + str(otooley_percentage) + "%  (" + str(otooley_votes) + ")")
+    text_file.write("-------------------------")
+    text_file.write("Winner:  " + str(max_name))
+    text_file.write("-------------------------")
+
